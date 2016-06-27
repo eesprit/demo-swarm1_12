e75a2ee2d9da4bcc0d437cf183032409b4a95972
Yann Cezard <yann.cezard@objectif-libre.com>
2016-06-27 15:49:49 +0200

    add color customisation

--- frontend/index.php
+++ frontend/index.php
@@ -1,17 +1,22 @@
 <?php
-  $color="green";
+  $color= getenv('COLOR') ? getenv('COLOR') : "green";
   $backend_url="http://backend/index.php";
+
+  function get_hostname() {
+    global $color;
+    return "<font color=\"" . $color . "\">" . gethostname() . "</font>";
+  }
 
   function call_backend() {
     global $backend_url;
-    $worker = $payload = file_get_contents($backend_url);
+    $worker = file_get_contents($backend_url);
     return $worker;
   }
 ?>
 <html><head><title>Swarm Service Discovery and Load Balancing Demo</title></head>
 <body>
-<h2>Traitement frontend effectu&eacute; par <font color="green"><?php echo gethostname(); ?></font></h2>
-<h2>Traitement backend effectu&eacute; par : <font color="red"><?php echo call_backend(); ?></font></h2>
+<h2>Traitement frontend effectu&eacute; par : <?php echo get_hostname(); ?></h2>
+<h2>Traitement backend effectu&eacute; par : <?php echo call_backend(); ?></h2>
 </body>
 </html>
 
